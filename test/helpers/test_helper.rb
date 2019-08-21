5adb7fac95201a1bcee9420f4c04660424676aa8
Joseph <jaymeagalli@gmail.com>
2019-08-21 22:46:23 +1000

    finish layout and routes

--- test/helpers/test_helper.rb
+++ test/helpers/test_helper.rb
@@ -7,6 +7,9 @@ Minitest::Reporters.use!
 class ActiveSupport::TestCase
 #Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
-
+    include ApplicationHelper
     #add more helper methods to be used by all tests here...
+
+    test
+
 end
